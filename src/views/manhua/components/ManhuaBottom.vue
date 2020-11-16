5fcff779c1bb1aba33cf760df127e3ec68ab9957
wangxi <wx925187390@163.com>
2020-11-16 17:03:07 +0800

    修改漫画部分-添加底部

---
+++ src/views/manhua/components/ManhuaBottom.vue
@@ -0,0 +1,131 @@
+<template>
+    <div class="manhua-bottom">
+        <div class="container">
+            <div class="left">
+                <img src="../../../CSS-manhua/bottom-1.png" height="78px" width="255px"/>
+                <div class="a-href">
+                    <a href="#">关于我们</a>
+                    <a href="#">联系我们</a>
+                    <a href="#">友情链接</a>
+                    <a href="#">加入我们</a>
+                </div>
+            </div>
+            <div style="width: 1px;
+            height: 88px;
+            background-color: hsla(0,0%,100%,.2);"></div>
+            <div class="right">
+                <a href="#">
+                    <img src="../../../CSS-manhua/bzgw-logo.png" height="66" width="67"/>
+                    <p>B 站官号</p>
+                    <div><div class="sanjiao"></div>关注即送券</div>
+                </a>
+                <a href="#">
+                    <img style="opacity: .75;" src="../../../CSS-manhua/xlwb-logo.png" height="64" width="64"/>
+                    <p>新浪微博</p>
+                </a>
+                <a href="#">
+                    <img style="opacity: .75;" src="../../../CSS-manhua/xz-logo.png" height="64" width="64"/>
+                    <p>下载bilibili客户端</p>
+                </a>
+            </div>
+            <div class="bottom">
+                <img src="../../../CSS-manhua/bottom-2.png" height="55" width="200"/>
+                <div style="color: hsla(0,0%,100%,.4);font-size: 12px ">
+                    <p>互联网 ICP 备案：沪 ICP17039440 号 - 1 | 网络文化经营许可证：沪网文 [2017]10251-781 号</p>
+                    <p>全国网安备案号：31011002003092 | 增值电信业务经营许可证 沪 B2-20190879</p>
+                    <p>12318 全国文化市场举报网站|上海市互联网违法和不良信息举报中心</p>
+                    <p>上海呵呵呵文化传播有限公司 | 地址：上海市杨浦区政立路485号 | 电话：021-25099888</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "ManhuaBottom"
+    }
+</script>
+
+<style scoped>
+    .manhua-bottom{
+        margin-top: 50px;
+        background: #212121;
+        padding-top: 40px;
+        padding-bottom: 20px;
+    }
+    .container {
+        width: 1160px;
+        margin: 0 auto;
+        display: flex;
+        flex-wrap: wrap;
+    }
+    .left{
+        width: 660px;
+        display: flex;
+        font-size: 14px;
+    }
+    .a-href{
+        width: 260px;
+        margin-left: 30px;
+    }
+    .a-href a{
+        display: inline-block;
+        margin: 10px 30px;
+        color: hsla(0,0%,100%,.6);
+        text-decoration: none;
+    }
+    .right{
+        display: flex;
+        margin-left: 50px;
+
+    }
+    .right a{
+        position: relative;
+        margin: 0 38px;
+    }
+    .right a p{
+        position: absolute;
+        left: 50%;
+        transform: translate(-50%);
+        white-space: nowrap;
+        color: hsla(0,0%,100%,.4);
+        font-size: 12px;
+    }
+    .right  a div{
+        padding: 2px 6px;
+        left: 50%;
+        top: 130%;
+        transform: translate(-50%);
+        border-radius: 2px;
+        background-color: #fb7299;
+        color: #fff;
+        white-space: nowrap;
+        font-size: 12px;
+        position: absolute;
+    }
+    /*.sanjiao{*/
+    /*    clear: both;*/
+    /*    background-color:#212121;*/
+    /*    height: 0;*/
+    /*    width: 0;*/
+    /*    content: "";*/
+    /*    display: block;*/
+    /*    border: 4px solid #fb7299;*/
+    /*    border-top: none;*/
+    /*    border-right-color: transparent;*/
+    /*    border-left-color: transparent;*/
+    /*    position: absolute;*/
+    /*    top:-50px;*/
+    /*    left: 50%;*/
+    /*    transform: translate(-50%,-100%);*/
+    /*}*/
+    .bottom {
+        display: flex;
+        margin-top: 30px;
+    }
+    .bottom img{
+        margin: 35px 30px 0 0;
+        display: block;
+    }
+</style>
---
+++ src/views/manhua/components/ManhuaMessage.vue
@@ -0,0 +1,118 @@
+<template>
+    <div class="manhua-message">
+        <div class="container">
+            <div class="title">
+                <span>热门速递</span>
+            </div>
+            <div class="main">
+                <div class="type">
+                    <a href="#"></a>
+                    <div class="type-title">原来我是恋爱游戏里的工具</div>
+                    <span style="font-size: 12px;color: #999999">后宫 搞笑 奇幻</span>
+                </div>
+                <div class="type">
+                    <a href="#"></a>
+                    <div class="type-title">到了30岁还是童贞的话，好像就会变成魔法使</div>
+                    <span style="font-size: 12px;color: #999999">恋爱</span>
+                </div>
+                <div class="type">
+                    <a href="#"></a>
+                    <div class="type-title">本剑仙绝不吃软饭</div>
+                    <span style="font-size: 12px;color: #999999">玄幻 搞笑 都市</span>
+                </div>
+                <div class="type">
+                    <a href="#"></a>
+                    <div class="type-title">百炼成神</div>
+                    <span style="font-size: 12px;color: #999999">奇幻 热血</span>
+                </div>
+                <div class="type">
+                    <a href="#"></a>
+                    <div class="type-title">我独自盗墓</div>
+                    <span style="font-size: 12px;color: #999999">热血 冒险 奇幻</span>
+                </div>
+                <div class="type">
+                    <a href="#"></a>
+                    <div class="type-title">废女妖神</div>
+                    <span style="font-size: 12px;color: #999999">古风 恋爱 玄幻</span>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "ManhuaMessage"
+    }
+</script>
+
+<style scoped>
+    .main{
+        /*height: 500px;*/
+    }
+    .container {
+        width: 1160px;
+        margin: 0 auto;
+    }
+    .title span{
+        display: inline-block;
+        font-size: 32px;
+        line-height: 45px;
+        padding: 60px 0 16px;
+        color: rgba(0,0,0,.87);
+    }
+
+    .main{
+        display: flex;
+        overflow: hidden;
+        flex-wrap: wrap;
+    }
+
+    .type{
+        padding: 0 20px 24px 0;
+        width: 16.6%;
+        box-sizing: border-box;
+    }
+    .type:nth-of-type(6n){
+        padding-right: 0;
+    }
+
+    .type a{
+        display: inline-block;
+        height: 240px;
+        width: 180px;
+        background-size: cover;
+        background-repeat: no-repeat;
+        background-position: 50%;
+    }
+    .type:first-of-type a{
+        background-image: url("//i0.hdslb.com/bfs/manga-static/a7dc676429bf6342e5d275a7b797465651165d4f.jpg@360w_480h.jpg")
+    }
+    .type:nth-of-type(2) a{
+        background-image: url("//i0.hdslb.com/bfs/manga-static/0e1c13d444d30e3cf192a853d3b87011b7ed4073.jpg@360w_480h.jpg");
+    }
+    .type:nth-of-type(3) a{
+        background-image: url("//i0.hdslb.com/bfs/manga-static/5d065f26899dc31eb8fec4be4f8ca85a804e0a06.jpg@360w_480h.jpg");
+    }
+    .type:nth-of-type(4) a{
+        background-image: url("//i0.hdslb.com/bfs/manga-static/bbd258a4123ddf8631727e3337dc2a5c99d2f087.jpg@360w_480h.jpg");
+    }
+    .type:nth-of-type(5) a{
+        background-image: url("//i0.hdslb.com/bfs/manga-static/9042fb75ddd325061fa4c221ac203e7a3b8c186f.jpg@360w_480h.jpg");
+    }
+    .type:nth-of-type(6) a{
+        background-image: url("//i0.hdslb.com/bfs/manga-static/5a8d061a2d112c6f96767e82d68bd87fc208e7a7.jpg@360w_480h.jpg");
+    }
+
+    .type-title{
+        width: 180px;
+        box-sizing: border-box;
+        height: 35px;
+        line-height: 35px;
+        font-size: 18px;
+        overflow: hidden; /*设置超出的部分进行影藏*/
+        text-overflow: ellipsis; /*设置超出部分使用省略号*/
+        white-space:nowrap ;
+    }
+
+</style>
